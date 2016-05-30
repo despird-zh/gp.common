38274d20d66733d89142f439857a78c7d20f68b5
gary-dgc <garydiao@localhost>
2016-05-30 17:45:52 +0800

    update

--- src/main/java/com/gp/pagination/PageWrapper.java
+++ src/main/java/com/gp/pagination/PageWrapper.java
@@ -16,18 +16,30 @@ public class PageWrapper<B>{
 	
 	private List<B> rows = null;
 
+	/**
+	 * Get the pagination information 
+	 **/
 	public PaginationInfo getPagination() {
 		return pagination;
 	}
 
+	/**
+	 * Set the pagination information 
+	 **/
 	public void setPagination(PaginationInfo pagination) {
 		this.pagination = pagination;
 	}
 
+	/**
+	 * Get the rows of data 
+	 **/
 	public List<B> getRows() {
 		return rows;
 	}
 
+	/**
+	 * Set the rows of data 
+	 **/
 	public void setRows(List<B> rows) {
 		this.rows = rows;
 	}	
